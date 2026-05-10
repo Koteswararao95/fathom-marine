6d5052b8195d620bf6475f4238ee93f89d8a3ed4
Koteswararao95 <koteswararaokampasati@gmail.com>
2026-05-10 23:45:30 +0530

    Updated production backend URL

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://fathom-marine-2lyp.onrender.com',
 });
 
 API.interceptors.request.use((config) => {
